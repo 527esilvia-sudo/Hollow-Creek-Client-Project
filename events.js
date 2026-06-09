49cd2355877fa16f7b7db87168089b87e5fe00c1
527esilvia-sudo <527Esilvia@frhsd.com>
2026-06-09 10:41:29 -0400

    need to finish every page- organize js files, read rubric

---
+++ events.js
@@ -0,0 +1,88 @@
+
+const events = document.getElementById('event-list');
+const today = new Date();
+
+const day = today.getDate();
+const month = today.getMonth();
+const year = today.getFullYear();
+
+function seasons() {
+    if (month >= 2 && month <= 4) {
+               console.log('Spring');
+        return 'Spring';
+     
+    } else if (month >= 5 && month <= 7) {
+        console.log('Summer');
+        return 'Summer';
+    }   else if (month >= 8 && month <= 10) {
+        return 'Fall';
+    } else {
+        return 'Winter';
+    }
+}
+
+if (seasons() === 'Summer') {
+   events.innerHTML = "";
+
+springEvents.forEach(item => {
+    events.innerHTML += `
+    <div class="card col-3 ms-5 me-5 mt-5 mb-5">
+        <img class="card-img-top" src="${item.img}" alt="${item.item}" />
+        <div class="card-body">
+            <h4 class="card-title">${item.item}</h4>
+            <p class="card-text">${item.note}</p>
+            <p class="card-text">${item.description}</p>
+        </div>
+    </div>
+    `;
+});
+}
+if (seasons() === 'Spring') {
+    events.innerHTML = "";
+
+    summerEvents.forEach(item => {
+        events.innerHTML += `
+        <div class="card col-3 ms-5 me-5 mt-5 mb-5">
+            <img class="card-img-top" src="${item.img}" alt="${item.item}" />
+            <div class="card-body">
+                <h4 class="card-title">${item.item}</h4>
+                <p class="card-text">${item.note}</p>
+                <p class="card-text">${item.description}</p>
+            </div>
+        </div>
+        `;
+    });
+}
+if (seasons() === 'Fall') {
+    events.innerHTML = "";
+
+    fallEvents.forEach(item => {
+        events.innerHTML += `
+        <div class="card col-3 ms-5 me-5 mt-5 mb-5">
+            <img class="card-img-top" src="${item.img}" alt="${item.item}" />
+            <div class="card-body">
+                <h4 class="card-title">${item.item}</h4>
+                <p class="card-text">${item.note}</p>
+                <p class="card-text">${item.description}</p>
+            </div>
+        </div>
+        `;
+    });
+}
+if (seasons() === 'Winter') {
+    events.innerHTML = "";
+
+    winterEvents.forEach(item => {
+        events.innerHTML += `
+        <div class="card col-3 ms-5 me-5 mt-5 mb-5">
+            <img class="card-img-top" src="${item.img}" alt="${item.item}" />
+            <div class="card-body">
+                <h4 class="card-title">${item.item}</h4>
+                <p class="card-text">${item.note}</p>
+                <p class="card-text">${item.description}</p>
+            </div>
+        </div>
+        `;
+    });
+}
+seasons()
--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+
+
+
+
 const card = document.getElementById('row-items');
 storeItems.forEach(item => {
 card.innerHTML += `
 
 <div class="card col-3 ms-5 me-5 mt-5 mb-5">
-    <img class="card-img-top" src="${item.img}" alt="${storeItems.item}" />
+    <img class="card-img-top" src="${item.img}" alt="${item.item}" />
     <div class="card-body">
         <h4 class="card-title">${item.item}</h4> 
         <p class="card-text">${item.price}</p> <br>
@@ -12,20 +16,4 @@ card.innerHTML += `
     </div>
 </div>`
 })
-function getSeason() {
-    const now = new Date();
-    const month = now.getMonth(); // 0 = Jan, 11 = Dec
 
-    // Northern Hemisphere Logic
-    if (month >= 2 && month <= 4) {
-        return "Spring"; // Mar, Apr, May
-    } else if (month >= 5 && month <= 7) {
-        return "Summer"; // Jun, Jul, Aug
-    } else if (month >= 8 && month <= 10) {
-        return "Autumn"; // Sep, Oct, Nov
-    } else {
-        return "Winter"; // Dec, Jan, Feb
-    }
-}
-
-console.log(`The current season is: ${getSeason()}`);
